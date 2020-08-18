5dfb9db8cdc4e7cf38c593941fa9750a92ebdee1
DanielBravo249 <69353269+DanielBravo249@users.noreply.github.com>
2020-08-18 09:57:12 -0400

    Add files via upload

---
+++ salario.cpp
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float s,j,d;
+    cout<<"Ingresar los dias"<<endl;
+    cin>>d;
+   cout<<"El monto del jornal"<<endl;
+    cin>>j;
+    s=d*j;
+    cout<<"El salario es: "<<s;
+    return 0;
+}
